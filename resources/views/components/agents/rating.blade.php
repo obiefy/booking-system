81a10fe176c17cebcfb04edbbb61737e60f58353
obiefy <obayhamed@gmail.com>
2018-11-14 11:34:09 +0200

    Show dynamic rating

--- resources/views/components/agents/rating.blade.php
+++ resources/views/components/agents/rating.blade.php
@@ -31,8 +31,49 @@
   </label>
 </form> -->
 
+@php
+    $rating = rand(1,5);
+@endphp
+
+@if($rating == 1)
+
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star "></span>
+<span class="fa fa-star "></span>
+<span class="fa fa-star"></span>
+<span class="fa fa-star"></span>
+
+@elseif($rating == 2)
+
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star"></span>
+<span class="fa fa-star"></span>
+<span class="fa fa-star"></span>
+
+
+@elseif($rating == 3)
+
 <span class="fa fa-star checked"></span>
 <span class="fa fa-star checked"></span>
 <span class="fa fa-star checked"></span>
 <span class="fa fa-star"></span>
 <span class="fa fa-star"></span>
+
+
+@elseif($rating == 4)
+
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star"></span>
+
+@elseif($rating == 5)
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+<span class="fa fa-star checked"></span>
+
+@endif
